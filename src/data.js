a0a62ef6596467cec53fd3053bece717acb3f94a
Manas_Goyal <manas.goyal@sleevesup.com.au>
2023-08-28 16:43:30 +0530

    resume-updated

--- src/data.js
+++ src/data.js
@@ -25,8 +25,7 @@ const greeting = {
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / React Native ."
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/1MUa6LTzoa3vZdwA4EYhDR3GywHUO4o3F/view?usp=drivesdk", // Set to empty to hide the button
+  resumeLink: "", // Set to empty to hide the button
   displayGreeting: true, // Set false to hide this section, defaults to true
 };
 
